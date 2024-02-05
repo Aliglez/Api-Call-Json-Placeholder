d392aef9475e66affb7388fa701de53ee24dd49c
Alicia González Castro <aliglezcastro@gmail.com>
2024-02-05 19:09:56 +0100

    created table

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -10,15 +10,33 @@ userStore.setUsers();
 
 <template>
     <div class="users">
-        <div v-if="userStore.isLoaded" class="users-card" v-for="users in userStore.users" :key="users.id">
-            <h2>Nombre: {{ users.name }}</h2>
-            <h2>Username: {{ users.username }}</h2>
-            <h2>Email: {{ users.email }}</h2>
-
-        </div>
+                
+            <tr>
+                <div v-if="userStore.isLoaded" class="users-card" v-for="users in userStore.users" :key="users.id" id="data_contain">
+                    <td id="celda"><h3>{{ users.name }}</h3></td>
+                    <td id="celda"><h3>{{ users.username }}</h3></td>
+                    <td id="celda"><h3>{{ users.email }}</h3></td>
+                </div>
+            </tr>
+           
         
- 
     </div>
 </template>
 <style scoped>
+
+
+tr, .users-card {
+    width: 100%;
+}
+
+#celda, th {
+    width: 15rem;
+}
+
+td {
+    width: 15rem;
+    border: black solid 1px;
+}
+
+
 </style>
